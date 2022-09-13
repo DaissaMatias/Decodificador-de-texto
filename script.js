6fd83a4c7d6ebef2cc0f4d127359db66a8a2aa99
Daissa <daissamatias@gmail.com>
2022-09-13 10:42:27 -0300

    Adicionando as constantes necessárias no código

---
+++ script.js
@@ -0,0 +1,7 @@
+const input = document.querySelector("#input-texto");
+const btnCriptografar = document.querySelector("#btn-criptografar");
+const btnDescriptografar = document.querySelector("#btn-descriptografar");
+const mensagem = document.querySelector("#mensagem");
+const btnCopiar = document.querySelector("#btn-copiar");
+
+document.getElementById("div-aparece").style.display = 'none';
